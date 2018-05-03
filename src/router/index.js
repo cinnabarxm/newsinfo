81b5a493004d7ea299115cf709e27bc3639a4892
zl <2744904518@qq.com>
2018-05-03 20:31:07 +0800

    完成部分路由规则以及组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,46 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import  Home from "../components/tabbar/home"
+import  Cart from "../components/tabbar/cart"
+import  Member from "../components/tabbar/member"
+import  Search from "../components/tabbar/search"
+
+import NewsDetail from "../components/news/detail"
+import NewsList from "../components/news/list"
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path:"/",
+      redirect:"/home"
+    },
+    {
+      path:"/home",
+      component:Home
+    },
+    {
+      path:"/cart",
+      component:Cart
+    },
+    {
+      path:"/member",
+      component:Member
+    },
+    {
+      path:"/search",
+      component:Search
+    },
+    {
+      path:"/newsdetail/:id",
+      component:NewsDetail
+    },
+    {
+      path:"/newslist",
+      component:NewsList
     }
   ]
 })
